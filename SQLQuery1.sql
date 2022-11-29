3753925f9b6b922f300f4d6d5416ee535f82726c
DenaRemawi <dremawi9991@gmail.com>
2022-11-30 01:31:33 +0300

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+create Database ACsTeches
+
+
+
+
+
+
---
+++ SQLQuery6.sql
@@ -0,0 +1,10 @@
+create Table UserDetiels(
+ID int primary key identity(1,1),
+FirstName nvarchar(max) null,
+FinalName nvarchar(max) null,
+UserName nvarchar(max) null,
+EmailUser nvarchar(max) null,
+MobileNumber nvarchar(max) null
+)
+
+
